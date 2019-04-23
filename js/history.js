ecb1c7d9603c9238343dd317474223ceb354e341
Courtney Solari <csolari@ufl.edu>
2019-04-22 22:15:56 -0500

    Added content and styles for History page

---
+++ js/history.js
@@ -0,0 +1,22 @@
+/*var photos = [
+              "../images/dream.jpg", 
+              "../images/old_west.jpg",
+              "../images/qh_race.jpg",
+              "../images/qh_rope.jpg",
+              "../images/PeterMcCue.jpg"
+            ];
+var placeholder = document.getElementById("slide-placeholder");
+var imageNum = 0;
+
+changePhoto();
+
+function changePhoto() {
+
+  var imageSrc = photos[imageNum];
+  placeholder.src=imageSrc;
+  imageNum++;
+  if (imageNum > 4) {
+    imageNum = 0;
+  }
+  setTimeout(changePhoto, 2000);
+}*/
