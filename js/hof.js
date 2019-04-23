304a32269daefcbaf26e5799784dabf230ebcc8e
Courtney Solari <csolari@ufl.edu>
2019-04-22 20:10:30 -0500

    Added styling and image gallery to Hall of Fame Page

--- js/hof.js
+++ js/hof.js
@@ -1,10 +1,50 @@
-$("#wimpy").animatedModal();
-$("#king").animatedModal();
-$("#leo").animatedModal();
-$("#3bars").animatedModal();
-$("#oldsorrel").animatedModal();
-$("#poco").animatedModal();
-$("#doc").animatedModal();
-$("#traveler").animatedModal();
-$("#sanpep").animatedModal();
-$("#skipw").animatedModal();
+$("#wimpy").animatedModal({
+	animatedIn: 'slideInLeft',
+	animatedOut: 'slideOutLeft',
+	color: '#f44237'
+});
+$("#king").animatedModal({
+	animatedIn: 'slideInLeft',
+	animatedOut: 'slideOutLeft',
+	color: '#f44237'
+});
+$("#leo").animatedModal({
+	animatedIn: 'slideInLeft',
+	animatedOut: 'slideOutLeft',
+	color: '#f44237'
+});
+$("#3bars").animatedModal({
+	animatedIn: 'slideInLeft',
+	animatedOut: 'slideOutLeft',
+	color: '#f44237'
+});
+$("#oldsorrel").animatedModal({
+	animatedIn: 'slideInLeft',
+	animatedOut: 'slideOutLeft',
+	color: '#f44237'
+});
+$("#poco").animatedModal({
+	animatedIn: 'slideInLeft',
+	animatedOut: 'slideOutLeft',
+	color: '#f44237'
+});
+$("#doc").animatedModal({
+	animatedIn: 'slideInLeft',
+	animatedOut: 'slideOutLeft',
+	color: '#f44237'
+});
+$("#traveler").animatedModal({
+	animatedIn: 'slideInLeft',
+	animatedOut: 'slideOutLeft',
+	color: '#f44237'
+});
+$("#sanpep").animatedModal({
+	animatedIn: 'slideInLeft',
+	animatedOut: 'slideOutLeft',
+	color: '#f44237'
+});
+$("#skipw").animatedModal({
+	animatedIn: 'slideInLeft',
+	animatedOut: 'slideOutLeft',
+	color: '#f44237'
+});
--- js/main.js
+++ js/main.js
@@ -10,4 +10,10 @@ var slideout = new Slideout({
     slideout.toggle();
   });
 
-  
+var image = document.getElementById("banner-img");
+var instance = new simpleParallax(image, {
+	scale: 1.25,
+	overflow: true,
+	orientation: 'down',
+	delay: 0.8
+});
